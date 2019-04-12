400583444152f62118fa61836574f0b2ee26a7dd
Adrian Kokot <splateusz@gmail.com>
2019-04-12 20:34:50 +0200

    layout

---
+++ RozkladJazdyPociagow-AplikacjaBazodanowa/Home.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace RozkladJazdyPociagow_AplikacjaBazodanowa
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnSearchPanel = new System.Windows.Forms.Button();
+            this.btnTrainPanel = new System.Windows.Forms.Button();
+            this.btnStationPanel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.searchPanel = new RozkladJazdyPociagow_AplikacjaBazodanowa.search();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.SteelBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 400);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.DarkGray;
+            this.pictureBox3.Location = new System.Drawing.Point(195, 0);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(5, 400);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnSearchPanel
+            // 
+            this.btnSearchPanel.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSearchPanel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearchPanel.FlatAppearance.BorderSize = 0;
+            this.btnSearchPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearchPanel.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchPanel.ForeColor = System.Drawing.Color.White;
+            this.btnSearchPanel.Location = new System.Drawing.Point(15, 62);
+            this.btnSearchPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSearchPanel.Name = "btnSearchPanel";
+            this.btnSearchPanel.Size = new System.Drawing.Size(180, 41);
+            this.btnSearchPanel.TabIndex = 5;
+            this.btnSearchPanel.Text = "Wyszukaj połączenia";
+            this.btnSearchPanel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSearchPanel.UseVisualStyleBackColor = false;
+            // 
+            // btnTrainPanel
+            // 
+            this.btnTrainPanel.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnTrainPanel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTrainPanel.FlatAppearance.BorderSize = 0;
+            this.btnTrainPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrainPanel.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrainPanel.ForeColor = System.Drawing.Color.White;
+            this.btnTrainPanel.Location = new System.Drawing.Point(15, 109);
+            this.btnTrainPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.btnTrainPanel.Name = "btnTrainPanel";
+            this.btnTrainPanel.Size = new System.Drawing.Size(180, 41);
+            this.btnTrainPanel.TabIndex = 6;
+            this.btnTrainPanel.Text = "Przebieg pociągu";
+            this.btnTrainPanel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTrainPanel.UseVisualStyleBackColor = false;
+            // 
+            // btnStationPanel
+            // 
+            this.btnStationPanel.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnStationPanel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStationPanel.FlatAppearance.BorderSize = 0;
+            this.btnStationPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStationPanel.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStationPanel.ForeColor = System.Drawing.Color.White;
+            this.btnStationPanel.Location = new System.Drawing.Point(15, 156);
+            this.btnStationPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.btnStationPanel.Name = "btnStationPanel";
+            this.btnStationPanel.Size = new System.Drawing.Size(180, 41);
+            this.btnStationPanel.TabIndex = 7;
+            this.btnStationPanel.Text = "Pociągi na stacji";
+            this.btnStationPanel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStationPanel.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.SteelBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 50);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.DarkGray;
+            this.pictureBox4.Location = new System.Drawing.Point(195, 50);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(600, 5);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Font = new System.Drawing.Font("Google Sans", 20F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(195, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 34);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Rozkład jazdy pociągów";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.SteelBlue;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(-37, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(231, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            // 
+            // searchPanel
+            // 
+            this.searchPanel.Location = new System.Drawing.Point(204, 58);
+            this.searchPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.searchPanel.Name = "searchPanel";
+            this.searchPanel.Size = new System.Drawing.Size(575, 300);
+            this.searchPanel.TabIndex = 13;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.searchPanel);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStationPanel);
+            this.Controls.Add(this.btnTrainPanel);
+            this.Controls.Add(this.btnSearchPanel);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Home";
+            this.Text = "Rozkład jazdy pociągów";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        
+        private System.Windows.Forms.Button btnSearchPanel;
+        private System.Windows.Forms.Button btnTrainPanel;
+        private System.Windows.Forms.Button btnStationPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private search searchPanel;
+    }
+}
+
---
+++ RozkladJazdyPociagow-AplikacjaBazodanowa/search.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace RozkladJazdyPociagow_AplikacjaBazodanowa
+{
+    partial class search
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstCity = new System.Windows.Forms.TextBox();
+            this.secondCity = new System.Windows.Forms.TextBox();
+            this.changeCities = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // firstCity
+            // 
+            this.firstCity.Location = new System.Drawing.Point(18, 21);
+            this.firstCity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.firstCity.Name = "firstCity";
+            this.firstCity.Size = new System.Drawing.Size(148, 27);
+            this.firstCity.TabIndex = 0;
+            // 
+            // secondCity
+            // 
+            this.secondCity.Location = new System.Drawing.Point(255, 30);
+            this.secondCity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.secondCity.Name = "secondCity";
+            this.secondCity.Size = new System.Drawing.Size(148, 27);
+            this.secondCity.TabIndex = 1;
+            // 
+            // changeCities
+            // 
+            this.changeCities.Location = new System.Drawing.Point(334, 135);
+            this.changeCities.Name = "changeCities";
+            this.changeCities.Size = new System.Drawing.Size(24, 23);
+            this.changeCities.TabIndex = 2;
+            this.changeCities.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(338, 236);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(163, 35);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Wyszukaj";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(60, 190);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(106, 27);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.changeCities);
+            this.Controls.Add(this.secondCity);
+            this.Controls.Add(this.firstCity);
+            this.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "search";
+            this.Size = new System.Drawing.Size(575, 300);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstCity;
+        private System.Windows.Forms.TextBox secondCity;
+        private System.Windows.Forms.Button changeCities;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
